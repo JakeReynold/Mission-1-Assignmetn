21803afae4cd2324a6b82bffd780af72b1554dca
JakeReynold <90359544+JakeReynold@users.noreply.github.com>
2022-01-17 14:33:48 -0700

    configure startup

--- Mission 1 Assignmetn/Startup.cs
+++ Mission 1 Assignmetn/Startup.cs
@@ -13,16 +13,26 @@ namespace Mission_1_Assignmetn
     public class Startup
     {
         // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        // For more information on how to configure your apjplication, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-          app.UseDefaultFiles();
-          app.UseStaticFiles();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Blajh}/{action=Index}/{id?}"
+                    );
+            });
         }
     }
 }
