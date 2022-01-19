d7f1a7c7a04a491c2dd0b46af377de16df91bbf4
JakeReynold <90359544+JakeReynold@users.noreply.github.com>
2022-01-19 10:30:51 -0700

    Final Changes

--- Mission 1 Assignmetn/Controllers/BlahController.cs
+++ Mission 1 Assignmetn/Controllers/BlahController.cs
@@ -9,17 +9,20 @@ namespace Mission_1_Assignmetn.Controllers
 {
     public class BlahController : Controller
     {
+        //Returns index page view
         public IActionResult Index()
         {
             return View();
         }
-
+        
+        //Returns the calculator page view
         [HttpGet]
         public IActionResult GradeCalculator()
         {
             return View();
         }
         
+        //Allow access to the calculator model
         [HttpPost]
         public IActionResult GradeCalculator(GradeCalculatorModel model)
         {
